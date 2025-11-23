fad6f4e20bde0e9301086b4648513e4b996cdc39
SrajanRai45 <112389449+SrajanRai45@users.noreply.github.com>
2025-11-23 19:34:42 +0530

    added extra questions with max-of-list.py

---
+++ src/extra/max-of-list.py
@@ -0,0 +1,19 @@
+def maximum(nums):
+    val = nums[0]
+    for i in nums:
+        if val < i:
+            val = i
+    return val
+
+
+nums = [float('-inf')]
+while True:
+    carray  = int(input("input the next element: "))
+    nums.append(carray)
+    print("\nAdd more elements?\n[Y or y] for yes or else for no: " , end = '')
+    temp = input()
+    print()
+    if temp not in ["y" , "Y"]:
+        break
+print(f"this is the maximum element: {maximum(nums)}")
+    
